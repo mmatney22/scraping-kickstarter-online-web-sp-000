b5e63ef9c03494697f4b0e0987d7855cfd2ee467
Morgan Matney <morgan.matney89@gmail.com>
2020-09-16 17:12:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,10 +23,3 @@ def create_project_hash
 end
 
 create_project_hash
-# projects: kickstarter.css("li.project.grid_4")
-# project.css("h2.bbcard_name strong a").text
-# project.css("div.project-thumbnail a img").attribute("src").value
-# project.css("p.bbcard_blurb").text
-# project.css("li.first funded strong").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
