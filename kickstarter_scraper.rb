de599409f522ab48f929b07eddc3e23b48efc632
Morgan Matney <morgan.matney89@gmail.com>
2020-09-16 16:49:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,26 @@
-# require libraries/modules here
+require "nokogiri"
+require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #Iterate through projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
+  #return projects hash
+  projects
 end
+
+create_project_hash
+# projects: kickstarter.css("li.project.grid_4")
+# project.css("h2.bbcard_name strong a").text
+# project.css("div.project-thumbnail a img").attribute("src").value
+# project.css("p.bbcard_blurb").text
+# project.css("li.first funded strong").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
